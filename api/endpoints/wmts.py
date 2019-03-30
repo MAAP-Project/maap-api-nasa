68fe1901b9990333b75329db3991a33ca6d60f22
Aimee Barciauskas <aimee@developmentseed.org>
2019-03-30 15:10:45 -0400

    Add missing imports

--- api/endpoints/wmts.py
+++ api/endpoints/wmts.py
@@ -1,8 +1,12 @@
 import logging
+import json
+import os
+import requests
 from flask import request
 from flask_restplus import Resource
 from api.restplus import api
 import api.utils.auth_util as auth
+from api import settings
 import api.endpoints.cmr as cmr
 from maap.Result import Granule
 
@@ -34,13 +38,13 @@ class GetTile(Resource):
 
         try:
             cmr_url = os.path.join(settings.CMR_URL, 'search', 'granules')
-            cmr_resp = requests.get(cmr_url, headers=cmr.get_search_headers(), params=cmr.parse_query_string())
-            granule = Granule(response.body, 'aws_access_key_id', 'aws_secret_access_key')
-            browse_image_url = granule._BrowseUrl
-            tile = requests.get('{}{}'.format(settings.BROWSE_URL, browse_image_url))
+            cmr_resp = requests.get(cmr_url, headers=cmr.get_search_headers(), params=cmr.parse_query_string(request.query_string))
+            print('cmr response {}'.format(cmr_resp.text))
+            granule = Granule(json.loads(cmr_resp.text)['feed']['entry'][0], 'aws_access_key_id', 'aws_secret_access_key')
+            urls = granule['links']
+            browse_image_url = list(filter(lambda x: x["title"] == "(BROWSE)", urls))[0]['href']
             response_body["message"] = "Successfully fetched browse image for {}".format(granule_ur)
-            ## Update me with resposne from fastbrowse api
-            response_body["tile"] = response.get("tile")
+            response_body["browse"] = browse_image_url
             response_body["code"] = 200
             response_body["success"] = True
         except Exception as ex:
