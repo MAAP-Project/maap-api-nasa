f9201d9f8380fa32b9e315e3c6128d69dc71a920
Aimee Barciauskas <aimee@developmentseed.org>
2019-03-31 20:17:07 -0400

    Use abdera

--- api/endpoints/wmts.py
+++ api/endpoints/wmts.py
@@ -11,8 +11,7 @@ from api import settings
 import api.endpoints.cmr as cmr
 from maap.Result import Granule
 from collections import OrderedDict
-from xmljson import BadgerFish
-bf = BadgerFish(dict_type=OrderedDict)
+from xmljson import abdera
 from xml.etree.ElementTree import fromstring
 
 log = logging.getLogger(__name__)
@@ -88,7 +87,7 @@ class GetCapabilities(Resource):
           'zoom': 10
         }
         xml_string = pystache.render(template, context)
-        return bf.data(fromstring(xml_string))
+        return abdera.data(fromstring(xml_string))
 
 
     # TODO(aimee): Revert this comment of @auth.token_required
