4aba7cbf74422fd17b8719c5ae92c6e7a5c97589
Aimee Barciauskas <aimee@developmentseed.org>
2019-04-12 18:17:31 -0400

    Fix import

--- api/endpoints/wmts.py
+++ api/endpoints/wmts.py
@@ -8,7 +8,7 @@ from api.restplus import api
 import api.utils.auth_util as auth
 from api import settings
 import api.endpoints.cmr as cmr
-import api.utils.Granule as Granule
+from api.utils.Granule import Granule
 from collections import OrderedDict
 import xmltodict
 
