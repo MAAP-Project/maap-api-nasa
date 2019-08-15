7d9722fdf051b9007a0648a422bb030d61a5c605
Malarout <namrata.malarout@jpl.nasa.gov>
2019-08-15 12:33:41 -0600

    malarout: updated settings file with lw job version

--- api/settings.py
+++ api/settings.py
@@ -39,6 +39,7 @@ CONTAINER_URL = "registry.nasa.maap.xyz/root/dps_plot:master"
 # HySDS Mozart
 MOZART_URL = "https://[MOZART_IP]/mozart/api/v0.2"
 DEFAULT_QUEUE = "test-job_worker-large"
+HYSDS_LW_VERSION = "v0.0.5"
 
 # FASTBROWSE API
 TILER_ENDPOINT = 'https://8e9mu91qr6.execute-api.us-east-1.amazonaws.com/production'
