c0ea53bfb53c1d2218828f858c53a31affaaa12d
bsatoriu <brian.p.satorius@jpl.nasa.gov>
2020-02-15 22:41:58 -0800

    Sqlalchemy update

--- api/maapapp.py
+++ api/maapapp.py
@@ -67,6 +67,7 @@ def initialize_app(flask_app):
     api.add_namespace(query_service_namespace)
     flask_app.register_blueprint(blueprint)
     db.create_all()
+    db.session.commit()
 
 
 def main():
