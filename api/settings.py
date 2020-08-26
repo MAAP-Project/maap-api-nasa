62c1913e5d118a985046f7432c423e5d8152e5ba
bsatoriu <brian.p.satorius@jpl.nasa.gov>
2020-08-26 13:56:10 -0700

    update 3d tile example urls

--- api/settings.py
+++ api/settings.py
@@ -4,7 +4,7 @@ MAAP_API_URL = "https://api.maap.xyz/api"
 APP_AUTH_KEY = "thisisthesecretkey"
 
 # Flask settings
-FLASK_SERVER_NAME = 'http://localhost:5000'
+FLASK_SERVER_NAME = 'localhost:5000'
 FLASK_DEBUG = True  # Do not use debug mode in production
 
 # Flask-Restplus settings
