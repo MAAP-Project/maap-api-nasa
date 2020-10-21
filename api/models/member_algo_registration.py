49fad9d1c76c1e675ee2a5961691822058b84f8b
namratam <namrataucg@gmail.com>
2020-10-20 17:19:58 -0700

    malarout: updated foreign keys

--- api/models/member_algo_registration.py
+++ api/models/member_algo_registration.py
@@ -1,7 +1,7 @@
 from api.models import Base
 from api.maap_database import db
 # from datetime import datetime
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow_sqlalchemy import SQLAlchemyAutoSchema, ForeignKey
 
 
 class MemberAlgorithmRegistration(Base):
@@ -10,13 +10,14 @@ class MemberAlgorithmRegistration(Base):
     id = db.Column(db.Integer, primary_key=True)
     member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
     username = db.Column(db.String, db.ForeignKey('member.id'), nullable=False)
-    email = db.Column(db.String, db.ForeignKey('member.id'), nullable=False)
+    email_id = db.Column(db.String, db.ForeignKey('member.id'), nullable=False)
     algorithm_key = db.Column(db.String())
     creation_date = db.Column(db.DateTime())
     commit_hash = db.Column(db.String())
     ade_webhook = db.Column(db.String())
-    member = db.relationship('Member', backref=db.backref('algorithms'))
-    username = db.relationship('Member', backref=db.backref('algorithms'))
+    member = db.relationship('Member', foreign_keys=[member_id],backref=db.backref('algorithms'))
+    user = db.relationship('Member', foreign_keys=[username], backref=db.backref('algorithms'))
+    email = db.relationship('Member', foreign_keys=[email_id], backref=db.backref('algorithms'))
 
     def __repr__(self):
         return "<MemberAlgorithmRegistration(registration_key={self.registration_key!r})>".format(self=self)
