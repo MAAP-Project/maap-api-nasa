61d31140a9a1ab979b3d357b2a06f765ff3fa8b9
Brian Satorius <brian.p.satorius@jpl.nasa.gov>
2022-05-10 13:47:39 -0700

    Member attribute additions

--- api/models/member.py
+++ api/models/member.py
@@ -16,6 +16,10 @@ class Member(Base):
     public_ssh_key_name = db.Column(db.String())
     public_ssh_key_modified_date = db.Column(db.DateTime())
     urs_token = db.Column(db.String())
+    status = db.Column(db.String())
+    gitlab_id = db.Column(db.String())
+    gitlab_username = db.Column(db.String())
+    gitlab_token = db.Column(db.String())
     creation_date = db.Column(db.DateTime())
 
     def __repr__(self):
