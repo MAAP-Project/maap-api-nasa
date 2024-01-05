40efb3e0b26bca71248009bff2c25b825787fd33
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2024-01-05 11:56:21 -0800

    added more print statments

--- api/utils/hysds_util.py
+++ api/utils/hysds_util.py
@@ -29,12 +29,14 @@ def get_mozart_job_info(job_id):
     
     try:
         products_staged = mozart_response["result"]["job"]["job_info"]["metrics"]["products_staged"]
+        logging.info(products_staged)
         if (len(products_staged) > 1):
             logging.info("graceal2 FOUND THE PRODUCTED STAGED TO BE OVER 1")
         product_url = mozart_response["result"]["job"]["job_info"]["metrics"]["products_staged"][0]["urls"][0]
         logging.info(product_url)
         dps_output_folder_name = "dps_output"
         index_folder_name = product_url.find("dps_output")
+        logging.info(index_folder_name)
         if (index_folder_name == -1):
             product_path = "Product path unavailable, folder output name must be "+dps_output_folder_name
         else:
