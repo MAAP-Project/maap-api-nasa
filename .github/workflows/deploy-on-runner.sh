837feb8e9a4d463ec6edce2109a6671c544bb60e
Frank Greguska <89428916+frankinspace@users.noreply.github.com>
2024-06-05 17:18:57 -0700

    attempt running deploy script

--- .github/workflows/deploy-on-runner.sh
+++ .github/workflows/deploy-on-runner.sh
@@ -1,15 +1,6 @@
 #!/usr/bin/env bash
 
 set -exo pipefail
-# Override settings with all MAAP_API_SETTINGS variables
-MAAP_API_SETTINGS_VARS=$(compgen -v | grep "MAAP_API_SETTINGS")
-for provided_setting_var in ${MAAP_API_SETTINGS_VARS}; do
-  var_name=${provided_setting_var#MAAP_API_SETTINGS_}
-  var_value=${!provided_setting_var}
-  #echo "Overriding ${var_name}"
-  echo "${var_name}=${var_value}" >> .maap-api.env
-  #sed -i "s|${var_name} = .*|${var_name} = '${var_value}'|g" settings.py
-done
 
 # # Make sure to add the public key of the CI machine to the authorized keys of the api machine
 ssh "${API_MACHINE}" "docker-compose -f docker-compose-maap-api.yml down"
