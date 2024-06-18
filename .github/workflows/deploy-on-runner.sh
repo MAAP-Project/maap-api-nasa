d35620b4b0ebbc3a1746a1d9caface91fd35b382
Frank Greguska <89428916+frankinspace@users.noreply.github.com>
2024-06-18 16:32:49 -0700

    use name ssh key

--- .github/workflows/deploy-on-runner.sh
+++ .github/workflows/deploy-on-runner.sh
@@ -3,11 +3,11 @@
 set -exo pipefail
 
 # # Make sure to add the public key of the CI machine to the authorized keys of the api machine
-ssh "${API_MACHINE}" "docker-compose -f docker-compose-maap-api.yml down"
+ssh -i ${API_MACHINE_KEYPATH} "${API_MACHINE}" "docker-compose -f docker-compose-maap-api.yml down"
 # Copy new file after compose down on api machine
 cat "${GITHUB_WORKSPACE}"/docker/docker-compose-maap-api.yml.tmpl | envsubst >> docker-compose-maap-api.yml
-scp -v docker-compose-maap-api.yml "${API_MACHINE}":~/
-scp -v .maap-api.env "${API_MACHINE}":~/.maap-api.env
+scp -i ${API_MACHINE_KEYPATH} -v docker-compose-maap-api.yml "${API_MACHINE}":~/
+scp -i ${API_MACHINE_KEYPATH} -v .maap-api.env "${API_MACHINE}":~/.maap-api.env
 
-ssh "${API_MACHINE}" "docker-compose -f docker-compose-maap-api.yml pull"
-ssh "${API_MACHINE}" "docker-compose -f docker-compose-maap-api.yml up -d"
+ssh -i ${API_MACHINE_KEYPATH} "${API_MACHINE}" "docker-compose -f docker-compose-maap-api.yml pull"
+ssh -i ${API_MACHINE_KEYPATH} "${API_MACHINE}" "docker-compose -f docker-compose-maap-api.yml up -d"
