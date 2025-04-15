e06b04f581bfb165b82505fd15156d89b9133981
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-04-15 11:59:26 -0700

    deleted old process files

--- api/schemas/process_old_schema.py
+++
@@ -1,9 +0,0 @@
-from api.models.processOld import ProcessOld
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
-
-
-class ProcessesSchema(SQLAlchemyAutoSchema):
-    class Meta:
-        model = ProcessOld
-        include_fk = True
-        load_instance = True
