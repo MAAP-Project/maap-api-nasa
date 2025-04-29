1731e550c73f46dc0dd66e17b45776141b88bdae
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2025-04-29 13:39:18 -0700

    implemented PUT for processes

--- api/endpoints/ogc.py
+++ api/endpoints/ogc.py
@@ -56,10 +56,6 @@ class Processes(Resource):
         print(existingProcesses)
 
         for process in existingProcesses:
-            print("graceal1 eixsting processes ")
-            print(process)
-            print(process.id)
-            print(process.process_id)
             existing_processes.append({'process_id': process.process_id,
                                        'id': process.id, 
                                        'version': process.version})
@@ -81,9 +77,11 @@ class Processes(Resource):
         req_data = json.loads(req_data_string)
         response_body = dict()
 
-        cwl_link = req_data.get("executionUnit").get("href")
         try:
-            response = requests.get(cwl_link).text
+            cwl_link = req_data.get("executionUnit").get("href")
+            response = requests.get(cwl_link)
+            response.raise_for_status()
+            response = response.text
         except:
             print("Error accessing cwl file")
             response_body["message"] = "Unable to access CWL"
@@ -102,7 +100,7 @@ class Processes(Resource):
         match = re.search(r"s:version:\s*(\S+)", response, re.IGNORECASE)
 
         if not match or not cwl_id:
-            print("Need to provide version at s:version or id")
+            response_body["code"] = status.HTTP_400_BAD_REQUEST
             response_body["message"] = "Need to provide version at s:version or id"
             print(match)
             print(cwl_id)
@@ -175,7 +173,7 @@ class Processes(Resource):
                 .query(Deployment_db) \
                 .filter_by(job_id=deployment_job_id) \
                 .first()
-            existingDeployment.status = "failed to submit to "+ existingDeployment.execution_venue
+            existingDeployment.status = "Failed to submit to "+ existingDeployment.execution_venue
             db.session.commit()
 
             response_body["code"] = status.HTTP_500_INTERNAL_SERVER_ERROR
@@ -193,14 +191,13 @@ class Processes(Resource):
         response_body["version"] = process_version
         response_body["deploymentJobsEndpoint"] = deploymentJobsEndpoint
 
-        process_pipeline_link = pipeline.web_url
-        response_body["processPipelineLink"] = process_pipeline_link
+        response_body["processPipelineLink"] = pipeline.web_url
 
         existingDeployment.status = "created" 
         
         db.session.commit()
         return response_body, status.HTTP_202_ACCEPTED
-  
+    
 @ns.route('/deploymentJobs/<int:job_id>')
 class Deployment(Resource):
 
@@ -325,3 +322,242 @@ class Describe(Resource):
         response_body["links"] = [{"href": existingProcess.cwl_link}]
         
         return response_body, status.HTTP_200_OK
+    
+    @api.doc(security='ApiKeyAuth')
+    @login_required()
+    def put(self, process_id):
+        """
+        replace an existing process
+        Must be the same user who posted the process 
+        :return:
+        """
+        response_body = dict()
+        try:
+            user = get_authorized_user()
+        except:
+            response_body["code"] = status.HTTP_500_INTERNAL_SERVER_ERROR
+            response_body["message"] = "Failed authenticate user"
+            return response_body, status.HTTP_500_INTERNAL_SERVER_ERROR
+            
+        # Get existing process 
+        existingProcess = db.session \
+                    .query(Process_db) \
+                    .filter_by(process_id=process_id) \
+                    .first()
+        
+        if existingProcess is None:
+            response_body["code"] = status.HTTP_404_NOT_FOUND
+            response_body["message"] = "No process with that process ID found"
+            return response_body, status.HTTP_404_NOT_FOUND 
+        
+        req_data_string = request.data.decode("utf-8")
+        req_data = json.loads(req_data_string)
+
+        print("graceal1 comparing the user ids")
+        print(user.id)
+        print(existingProcess.user)
+        # Make sure same user who originally posted process 
+        if user.id != existingProcess.user:
+            response_body["code"] = status.HTTP_403_FORBIDDEN
+            response_body["message"] = "You can only modify processes that you posted originally"
+            return response_body, status.HTTP_403_FORBIDDEN 
+        
+        try:
+            cwl_link = req_data.get("executionUnit").get("href")
+            response = requests.get(cwl_link)
+            response.raise_for_status()
+            response = response.text
+        except:
+            print("Error accessing cwl file")
+            response_body["message"] = "Unable to access CWL"
+            return response_body, status.HTTP_400_BAD_REQUEST
+        
+        # delete the previous entry from HySDS 
+        # try:
+        #     job_type = "job-{}:{}".format(existingProcess.id, existingProcess.version)
+        #     print("gracael1 about to delete the job in mozart")
+        #     hysds.delete_mozart_job_type(job_type)
+        #     print("graceal1 done deleting the job in mozart")
+        # except: 
+        #     response_body["code"] = status.HTTP_500_INTERNAL_SERVER_ERROR
+        #     response_body["message"] = "Failed to process request to delete {}".format(job_type)
+        #     return response_body, status.HTTP_500_INTERNAL_SERVER_ERROR
+        
+        # TODO right now this will make 2 requests to get the data and I should fix that later
+        # ideally need to save all the contents to a file then read from that for load document
+        # do saving and deleting of this file in try catch 
+        cwl_obj = load_document_by_uri(cwl_link, load_all=True)
+
+        workflow = None
+        for i in range(len(cwl_obj)):
+            if type(cwl_obj[i]) == cwl_v1_2.Workflow:
+                workflow = cwl_obj[i]
+        new_cwl_id = workflow.id
+        match = re.search(r"s:version:\s*(\S+)", response, re.IGNORECASE)
+
+        if not match or not new_cwl_id:
+            response_body["code"] = status.HTTP_400_BAD_REQUEST
+            response_body["message"] = "Need to provide version at s:version or id"
+            print(match)
+            print(new_cwl_id)
+            return response_body, status.HTTP_400_BAD_REQUEST
+        
+        fragment = urllib.parse.urlparse(new_cwl_id).fragment
+        new_cwl_id = os.path.basename(fragment)
+        new_process_version = match.group(1)
+
+        print("graceal1 New id and version are")
+        print(new_cwl_id)
+        print(new_process_version)
+
+        if new_cwl_id != existingProcess.id or new_process_version != existingProcess.version:
+            response_body["code"] = status.HTTP_400_BAD_REQUEST
+            response_body["message"] = "Need to provide same id and version as previous process which is {}:{}".format(existingProcess.id, existingProcess.version)
+            return response_body, status.HTTP_400_BAD_REQUEST
+
+        # need to create deployment before this call to get the job_id 
+        deployment = Deployment_db(created=datetime.now(),
+                                execution_venue=settings.DEPLOY_PROCESS_EXECUTION_VENUE, 
+                                status="submitted", # TODO not consistent with gitlab status endpoints I think, but can update later 
+                                cwl_link=cwl_link,
+                                user=user.id,
+                                id=existingProcess.id,
+                                version=existingProcess.version)
+        db.session.add(deployment)
+        db.session.commit()
+
+        # Get the deployment you just committed to access its now assigned job id 
+        deployment = db.session \
+                .query(Deployment_db) \
+                .filter_by(id=existingProcess.id,version=existingProcess.version,status="submitted") \
+                .first()
+
+        deployment_job_id = deployment.job_id
+        
+        deploymentJobsEndpoint = "/deploymentJobs/" + str(deployment_job_id)
+
+        # Post the process to the deployment venue 
+        try:
+            gl = gitlab.Gitlab(settings.GITLAB_URL_POST_PROCESS, private_token=settings.GITLAB_POST_PROCESS_TOKEN)
+            project = gl.projects.get(settings.GITLAB_PROJECT_ID_POST_PROCESS)
+            pipeline = project.pipelines.create({
+                'ref': settings.VERSION,
+                'variables': [
+                    {'key': 'CWL_URL', 'value': cwl_link},
+                    {'key': 'CALLBACK_URL', 'value': deploymentJobsEndpoint}
+                ]
+            })
+            print(f"Triggered pipeline ID: {pipeline.id}")
+        except: 
+            # TODO graceal make sure this is edited object correctly 
+            print("graceal failed to submit process to pipeline")
+            existingDeployment = db.session \
+                .query(Deployment_db) \
+                .filter_by(job_id=deployment_job_id) \
+                .first()
+            existingDeployment.status = "Failed to submit to "+ existingDeployment.execution_venue
+            db.session.commit()
+
+            response_body["code"] = status.HTTP_500_INTERNAL_SERVER_ERROR
+            response_body["message"] = "Failed to start CI/CD to deploy process. "+settings.DEPLOY_PROCESS_EXECUTION_VENUE+" is likely down"
+            return response_body, status.HTTP_500_INTERNAL_SERVER_ERROR
+
+        # Update the deployment you just created with the pipeline id and status from gitlab
+        existingDeployment = db.session \
+            .query(Deployment_db) \
+            .filter_by(job_id=deployment_job_id) \
+            .first()
+        existingDeployment.pipeline_id = pipeline.id
+
+        response_body["id"] = existingProcess.id
+        response_body["version"] = existingProcess.version
+        response_body["deploymentJobsEndpoint"] = deploymentJobsEndpoint
+
+        response_body["processPipelineLink"] = pipeline.web_url
+
+        existingDeployment.status = "created" 
+        
+        db.session.commit()
+        return response_body, status.HTTP_202_ACCEPTED
+  
+    
+# @ns.route('/processes/<string:process_id>/execution')
+# class Describe(Resource):
+
+#     @api.doc(security='ApiKeyAuth')
+#     @login_required()
+#     def post(self, process_id):
+#         """
+#         This posts a job to execute 
+#         Changes to OGC schema: 
+#         - adding queue to request body 
+#         :return:
+#         """
+#         print("graceal in the body of executing a job")
+#         req_data_string = request.data.decode("utf-8")
+#         req_data = json.loads(req_data_string)
+#         response_body = dict()
+
+#         existingProcess = db.session \
+#                     .query(Process_db) \
+#                     .filter_by(process_id=process_id) \
+#                     .first()
+#         if existingProcess is None:
+#             response_body["code"] = status.HTTP_404_NOT_FOUND
+#             response_body["message"] = "No process with that process ID found"
+#             return response_body, status.HTTP_404_NOT_FOUND 
+        
+#         inputs = req_data.get("inputs")
+#         print("graceal1 inputs from post are")
+#         print(inputs)
+#         job_type = "job-{}:{}".format(existingProcess.id, existingProcess.version)
+
+#         # validate the inputs provided by user against the registered spec for the job
+#         try:
+#             hysdsio_type = job_type.replace("job-", "hysds-io-")
+#             hysds_io = hysds.get_hysds_io(hysdsio_type)
+#             logging.info("Found HySDS-IO: {}".format(hysds_io))
+#             # graceal, should do this add add validation steps later 
+#             # params = hysds.validate_job_submit(hysds_io, input_params)
+#         except Exception as ex:
+#             response_body["code"] = status.HTTP_500_INTERNAL_SERVER_ERROR
+#             response_body["message"] = "Error validating inputs with HySDS"
+#             return response_body, status.HTTP_500_INTERNAL_SERVER_ERROR 
+
+#         try:
+#             user = get_authorized_user()
+#             queue = req_data.get("queue")
+#             queue = job_queue.validate_or_get_queue(queue, job_type, user.id)
+#             job_time_limit = hysds_io.get("result").get("soft_time_limit", 86400)
+#             if job_queue.contains_time_limit(queue):
+#                 job_time_limit = int(queue.time_limit_minutes) * 60
+#             # what is dedup?? 
+#             response = hysds.mozart_submit_job(job_type=job_type, params=inputs, dedup=dedup, queue=queue.queue_name,
+#                                                identifier="{}:{}".format(existingProcess.id, existingProcess.version), job_time_limit=int(job_time_limit))
+
+#             logging.info("Mozart Response: {}".format(json.dumps(response)))
+#             job_id = response.get("result")
+#             if job_id is not None:
+#                 logging.info("Submitted Job with HySDS ID: {}".format(job_id))
+#                 # the status is hard coded because we query too fast before the record even shows up in ES
+#                 # we wouldn't have a Job ID unless it was a valid payload and got accepted by the system
+#                 if response.get("orig_job_status") is not None:
+#                     job_status = response.get("orig_job_status")
+#                 else:
+#                     job_status = "job-queued"
+#                 self._log_job_submission(job_id, input_params)
+#                 return Response(ogc.status_response(job_id=job_id, job_status=job_status), mimetype='text/xml')
+#             else:
+#                 raise Exception(response.get("message"))
+#         except ValueError as ex:
+#             logging.error(traceback.format_exc())
+#             return Response(ogc.get_exception(type="FailedJobSubmit", origin_process="Execute",
+#                                               ex_message=str(ex)), status.HTTP_400_BAD_REQUEST)
+#         except Exception as ex:
+#             logging.info("Error submitting job: {}".format(ex))
+#             return Response(ogc.get_exception(type="FailedJobSubmit", origin_process="Execute",
+#                             ex_message="Failed to submit job of type {}. Exception Message: {}"
+#                             .format(job_type, ex)), status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+        
+#         return response_body, status.HTTP_202_ACCEPTED
